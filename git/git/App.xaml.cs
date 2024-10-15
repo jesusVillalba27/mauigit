32878ff3efe13c0346ffd9b669e69ba17d3ff0ce
jesusVillalba27 <jesus.villalbav@cecar.edu.co>
2024-10-14 22:24:01 -0500

    proyecto

---
+++ git/git/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace git
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ git/git/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace git
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
